df25e7ca4658c439a495df8bcbe5dcb018d605f0
Hernando17 <ginkotsu17@gmail.com>
2023-01-06 19:57:27 +0700

    Table fixed

--- src/pages/layout/dashboard/index.jsx
+++ src/pages/layout/dashboard/index.jsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
-import { SideBar, TopBar } from '../../../components';
-import "../../../assets/css/dashboard.css";
+import * as React from 'react';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 
-export default function DashboardLayout({ children, title }) {
+const columns = [
+    { field: 'id', headerName: '#', flex: 1 },
+    { field: 'firstName', headerName: 'Table', flex: 1 },
+    { field: 'lastName', headerName: 'Waitress', flex: 1 },
+    {
+        field: 'age',
+        headerName: 'Action',
+        flex: 1
+    },
+];
+
+const rows = [
+    { id: 1, lastName: 'Snsssw', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
+
+export function DataTable() {
     return (
-        <div className="flex bg-cream">
-            <HelmetProvider>
-                <Helmet>
-                    <meta charSet="utf-8" />
-                    <title>{title}</title>
-                </Helmet>
-            </HelmetProvider>
-
-            <SideBar />
-            <div className="flex flex-col flex-wrap w-full h-[100vh] overflow-auto">
-                <TopBar />
-                <div className="px-20 py-12">
-                    <h1 className="font-bold text-[24px] mb-4">{title}</h1>
-                    {children}
-                </div>
-            </div>
+        <div style={{ backgroundColor: "white", height: 400, width: '100%' }}>
+            <DataGrid
+                color="primary"
+                rows={rows}
+                columns={columns}
+                pageSize={20}
+                rowsPerPageOptions={[20]}
+                components={{ Toolbar: GridToolbar }}
+                componentsProps={{
+                    toolbar: {
+                        showQuickFilter: true,
+                        quickFilterProps: { debounceMs: 500 }
+                    }
+                }}
+            />
         </div>
-    )
+    );
 }
